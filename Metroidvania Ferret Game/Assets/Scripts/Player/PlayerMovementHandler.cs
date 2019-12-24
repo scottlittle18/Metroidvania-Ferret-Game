46862f94968fb8b68bcac76d0c514c39a06bc0fe
Scott Little <scott.little@loop.colum.edu>
2019-12-23 18:03:31 -0600

    Added Horizontal Movement and Sprite Flipping

--- Metroidvania Ferret Game/Assets/Scripts/Player/PlayerMovementHandler.cs
+++ Metroidvania Ferret Game/Assets/Scripts/Player/PlayerMovementHandler.cs
@@ -11,55 +11,74 @@ public class PlayerMovementHandler : MonoBehaviour
 {
     #region Serialized Fields
     [SerializeField]
-    private float playerMoveSpeed;
-    #endregion
+    private float m_playerMoveSpeed = 0f;
 
-    private Rigidbody2D playerRigidbody;
+    [SerializeField, Tooltip("This determines how close to zero the player's velocity needs to be to flip the sprite in the Sprite Renderer.")]
+    private float m_turningSpriteFlipThreshold = 0f;
+    #endregion------------
+
+    #region Standard Local Member Variables (m_ == A local member of a class)
+    private bool m_isfacingLeft;
+    #endregion------------
+
+    #region Component Variable Containers
+    private Rigidbody2D m_playerRigidbody;
+    private SpriteRenderer m_playerSpriteRenderer;
+    #endregion------------
 
     /// <summary>
-    /// Contains all movement related input required for this script.
+    /// An internal class that contains all movement related input fields required for this script.
     /// </summary>
     internal class InputListener
     {
         public float horizontalMoveInput;
     }
 
-    private InputListener inputListener = new InputListener();
+    private InputListener m_inputListener = new InputListener();
 
     // Start is called before the first frame update
     private void Start()
     {
         //Set up all necessary component variables
         InitializePlayerComponents();
-
     }
 
     // Update is called once per frame
     private void Update()
     {
+        //--Listeners--
         HorizontalMoveInputListener();
+
+        //If any input to the horizontal axis are detected, update the look direction to keep the sprite facing the last direction the player moved in
+        if (!Mathf.Approximately(m_inputListener.horizontalMoveInput, 0.0f))
+            UpdateLookDirection();
+
+        //TODO: Debugging PlayerMovementHandler.Update()
+        Debug.Log($"The player's current horizontal input is: {m_inputListener.horizontalMoveInput}\n" +
+            $"The current value of the player's m_isFacingLeft variable is: {m_isfacingLeft}");
     }
 
     private void FixedUpdate()
     {
         //Apply horizontal player movement input.
-        HorizontalMoveInputHandler();        
+        HorizontalMoveInputHandler();  
     }
 
-    /// <summary>
-    /// Listens for the input received from the player. This data is used by the HorizontalMoveInputHandler() to apply this to the movement of the player-character.
-    /// </summary>
-    private void HorizontalMoveInputListener()
-    {
-        inputListener.horizontalMoveInput = Input.GetAxisRaw("Horizontal");
-    }
-    
     /// <summary>
     /// Used to Initialize any necessary component variables (e.g. Rigidbody2D, BoxCollider2D, etc.)
     /// </summary>
     private void InitializePlayerComponents()
     {
-        playerRigidbody = GetComponent<Rigidbody2D>();
+        m_playerRigidbody = GetComponent<Rigidbody2D>();
+        m_playerSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
+    /// <summary>
+    /// Listens for the input received from the player. This data is used by the HorizontalMoveInputHandler() to apply this to the movement of the player-character.
+    /// </summary>
+    private void HorizontalMoveInputListener()
+    {
+        m_inputListener.horizontalMoveInput = Input.GetAxisRaw("Horizontal");
     }
 
     /// <summary>
@@ -67,6 +86,30 @@ public class PlayerMovementHandler : MonoBehaviour
     /// </summary>
     private void HorizontalMoveInputHandler()
     {
-        playerRigidbody.velocity = new Vector2(inputListener.horizontalMoveInput * playerMoveSpeed * Time.deltaTime, playerRigidbody.velocity.y);
+        m_playerRigidbody.velocity = new Vector2(m_inputListener.horizontalMoveInput * m_playerMoveSpeed * Time.deltaTime, m_playerRigidbody.velocity.y);
+    }
+
+    /// <summary>
+    /// Updates the direction the sprite should be facing based on the horizontal player input
+    /// </summary>
+    private void UpdateLookDirection()
+    {
+        if (m_inputListener.horizontalMoveInput > m_turningSpriteFlipThreshold)
+        {
+            m_playerSpriteRenderer.flipX = m_isfacingLeft = true;
+            m_isfacingLeft = true;
+        }
+        else if (m_inputListener.horizontalMoveInput < -m_turningSpriteFlipThreshold)
+        {
+            m_playerSpriteRenderer.flipX = false;
+            m_isfacingLeft = false;
+        }
+        else if (m_inputListener.horizontalMoveInput >= - m_turningSpriteFlipThreshold && m_inputListener.horizontalMoveInput <= m_turningSpriteFlipThreshold)
+        {
+            if (!m_isfacingLeft)
+                m_playerSpriteRenderer.flipX = true;
+            else
+                m_playerSpriteRenderer.flipX = false;
+        }
     }
 }
