6b656ae6e9130ddaac53fd372025d7d8ab9f3be2
Scott Little <scott.little@loop.colum.edu>
2020-01-02 17:31:32 -0600

    Changed the Ground check method back to the original way
    Changed back to using a box collider 2d on the GroundCheck object to check if the player is on the ground

--- Metroidvania Ferret Game/Assets/Scripts/Player/GroundCheck.cs
+++ Metroidvania Ferret Game/Assets/Scripts/Player/GroundCheck.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 /// <summary>
 /// Used to detect if the attached character is on the ground
 /// </summary>
+[RequireComponent(typeof(BoxCollider2D))]
 public class GroundCheck : MonoBehaviour
 {
-    [SerializeField, Tooltip("Adjusts the radius for the ground check object.")]
-    private float groundCheckRadius;
+    //[SerializeField, Tooltip("Adjusts the radius for the ground check object.")]
+    //private float groundCheckRadius;
 
-    [SerializeField, Tooltip("Used to specify which layer mask the ground check will look for.")]
-    private LayerMask whatIsGround;
+    //[SerializeField, Tooltip("Used to specify which layer mask the ground check will look for.")]
+    //private LayerMask whatIsGround;
+
+    private BoxCollider2D m_groundCheckCollider;
 
     //This allows the IsGrounded variable to be read from the associated object's movement script.
     private bool m_isGrounded;
@@ -21,9 +24,36 @@ public class GroundCheck : MonoBehaviour
         private set { m_isGrounded = value; }
     }
 
+    private void Awake()
+    {
+        m_groundCheckCollider = GetComponent<BoxCollider2D>();
+
+        // If the ground check collider was not set to be a trigger in the inspector, set it to be one
+        if (m_groundCheckCollider.isTrigger != true)
+        {
+            m_groundCheckCollider.isTrigger = true;
+        }
+    }
+
     private void Update()
     {
         //IsGrounded == true if the player is on an object in the "Ground" LayerMask.
-        IsGrounded = Physics2D.OverlapCircle(this.transform.position, groundCheckRadius, whatIsGround);
+        //IsGrounded = Physics2D.OverlapCircle(this.transform.position, groundCheckRadius, whatIsGround);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            IsGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            IsGrounded = false;
+        }
     }
 }
