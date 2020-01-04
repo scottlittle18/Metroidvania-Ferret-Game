75aff342ad7bf524867652d85ce272f683a0ebe1
Scott Little <scott.little@loop.colum.edu>
2020-01-04 16:10:18 -0600

    Made new script for rising platforms and added adjusted it to allow the player to stand on it

---
+++ Metroidvania Ferret Game/Assets/Scripts/RisingPlatformVelocityLimiter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisingPlatformVelocityLimiter : MonoBehaviour
+{
+    [SerializeField]
+    private float m_maxVelocity;
+
+    private Rigidbody2D m_platformBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_platformBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 clampedVelocity = m_platformBody.velocity;
+        clampedVelocity.y = Mathf.Clamp(m_platformBody.velocity.y, Mathf.NegativeInfinity, m_maxVelocity);
+        m_platformBody.velocity = clampedVelocity;
+    }
+}
