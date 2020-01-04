b659d65dd9165e9d29a52354e146997aea703e93
Scott Little <scott.little@loop.colum.edu>
2020-01-04 13:56:55 -0600

    Created PS_SteamPlume & PSController_SteamPlume
    Created:
    - A particle system for the steam plume.
    - PSController_SteamPlume.cs which controls the amount of time between particle emissions.
    
    Next Steps:
    - Clean up PSCntroller_SteamPlume.cs, HOWEVER, keep the Debug method in.
    - Add Comments and Summaries to PSController_SteamPlume.cs to increase readability

---
+++ Metroidvania Ferret Game/Assets/Scripts/PSController_SteamPlume.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PSController_SteamPlume : MonoBehaviour
+{
+    [SerializeField, Tooltip("How long to wait between plumes.")]
+    private float m_plumeCooldownTime = 0.0f;
+
+    private bool m_canPlay;
+    
+    private ParticleSystem m_steamPlumeSystem;
+
+    private IEnumerator SteamPlumeCooldown()
+    {
+        //m_steamPlumeSystem.Play();
+        yield return new WaitForSecondsRealtime(m_steamPlumeSystem.main.duration);
+        m_canPlay = false;
+        yield return new WaitForSecondsRealtime(m_plumeCooldownTime);
+        m_canPlay = true;
+    }
+
+    private void Awake()
+    {
+        m_steamPlumeSystem = GetComponent<ParticleSystem>();
+        m_canPlay = m_steamPlumeSystem.main.playOnAwake;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: Debug Method
+        Debugging();
+
+        var m_isEmitting = m_steamPlumeSystem.emission;
+        m_isEmitting.enabled = m_canPlay;
+
+        if (m_isEmitting.enabled == true && m_steamPlumeSystem.isEmitting == false)
+        {
+            m_steamPlumeSystem.Play();
+        }
+
+        if (m_canPlay)
+        {
+            StartCoroutine(SteamPlumeCooldown());
+        }
+
+        //if (m_canPlay)
+        //{
+        //    m_steamPlumeSystem.Play();
+        //    StartCoroutine(SteamPlumeCooldown());
+        //}
+
+        //if (!m_canPlay)
+        //{
+        //    m_steamPlumeSystem.Stop();
+        //}
+    }
+
+    private void Debugging()
+    {
+        //Debug.Log($"m_isEmitting == {m_isEmitting}");
+        Debug.Log($"m_canPlay == {m_canPlay}");
+        Debug.Log($"Emission.Enabled == {m_steamPlumeSystem.emission.enabled}");
+    }
+}
