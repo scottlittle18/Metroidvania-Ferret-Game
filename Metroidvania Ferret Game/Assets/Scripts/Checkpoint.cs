66bf1c97a6a577f5d51d2c34a25f1e045a075969
Scott Little <scott.little@loop.colum.edu>
2020-01-05 22:45:23 -0600

    Imported prototype checkpoint sprite

---
+++ Metroidvania Ferret Game/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Checkpoint : MonoBehaviour
+{    
+    private bool isActivated;   
+    private Animator anim;
+    private PlayerController player;
+    /// <summary>
+    /// Property Used to Activate or Deactivate checkpoints
+    /// </summary>
+    public bool IsActivated
+    {
+        get
+        {
+            return isActivated;
+        }
+        set
+        {
+            isActivated = value;
+            UpdateAnimation();
+        }
+    }
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        IsActivated = false;        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {            
+            player = collision.GetComponent<PlayerController>();
+            player.CurrentCheckpoint = this;
+        }
+    }
+
+    private void UpdateAnimation()
+    {
+        anim.SetBool("isActivated", IsActivated);
+    }
+}
